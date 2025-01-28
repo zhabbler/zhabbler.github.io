7723c9b8f2466a627f7436f095cbbcc120a27b3a
Yaroslav Kosygin <91693467+iamdelickkk@users.noreply.github.com>
2025-01-28 21:14:41 +0300

    api upd

--- docs/docs_static/js/main.js
+++ docs/docs_static/js/main.js
@@ -1,10 +1,28 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".navbar_top").style.top = "0";
-    } else {
-        document.querySelector(".navbar_top").style.top = "-55px";
+$(document).ready(function(){
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".navbar_top").style.top = "0";
+        } else {
+            document.querySelector(".navbar_top").style.top = "-55px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
-    }
+    $("h2,h3").each(function(){
+        if($(this).prop("tagName") == 'H2' && !$(this).hasClass("JS_IGn")){
+            $('#contain').append(`<li>
+                    <a href="#${$(this).html().replace(/\s/g, '').toLowerCase()}-obj">
+                        ${$(this).html()}
+                    </a>
+                </li><ul></ul>`);
+        }
+        if($(this).prop("tagName") == 'H3' && !$(this).hasClass("JS_IGn")){
+            $('#contain ul:last-child').append(`<li>
+                    <a href="#${$(this).html().replace(/\s/g, '').toLowerCase()}-func">
+                        ${$(this).html()}
+                    </a>
+                </li>`);
+        }
+    });
+});
